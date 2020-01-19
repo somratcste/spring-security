ce7e19e6539052e8f4a9bf08c96013ddd2a1fe2e
somratcste <somratcste@gmail.com>
2020-01-19 13:48:04 +0100

    configure permission based authorization

--- unsecure-application/src/main/java/rc/bootsecurity/config/SecurityConfiguration.java
+++ unsecure-application/src/main/java/rc/bootsecurity/config/SecurityConfiguration.java
@@ -17,11 +17,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
-                .withUser("admin").password(passwordEncoder().encode("admin123")).roles("ADMIN")
+                .withUser("admin")
+                .password(passwordEncoder().encode("admin123"))
+                .roles("ADMIN").authorities("ACCESS_TEST1", "ACCESS_TEST2")
                 .and()
-                .withUser("nazmul").password(passwordEncoder().encode("nazmul123")).roles("USER")
+                .withUser("nazmul")
+                .password(passwordEncoder().encode("nazmul123"))
+                .roles("USER")
                 .and()
-                .withUser("manager").password(passwordEncoder().encode("manager123")).roles("MANAGER");
+                .withUser("manager")
+                .password(passwordEncoder().encode("manager123"))
+                .roles("MANAGER");
     }
 
     @Override
@@ -32,7 +38,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/profile/**").authenticated()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/management/**").hasAnyRole("ADMIN", "MANAGER")
-                .antMatchers("/api/public/**").authenticated()
+                .antMatchers("/api/public/test1").hasAuthority("ACCESS_TEST1")
+                .antMatchers("/api/public/test2").hasAuthority("ACCESS_TEST2")
                 .and()
                 .httpBasic();
     }
